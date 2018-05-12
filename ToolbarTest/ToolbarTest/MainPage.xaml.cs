64e899362dcae7ad18e01fc3246290f7163754a8
karthick <foxkarthick@hotmail.com>
2018-05-12 12:15:27 +0800

    Add project files.

---
+++ ToolbarTest/ToolbarTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToolbarTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu1Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Menu Clicked", "", "Okay");
+        }
+    }
+}
